357359c9767a1605106618b6949dbc20998069aa
Peter <peter.graf@nrel.gov>
2014-02-13 11:30:05 -0700

    added model files for OC3 floating offshore turbine

--- src/AeroelasticSE/FSTTemplate_runner.py
+++ src/AeroelasticSE/FSTTemplate_runner.py
@@ -10,22 +10,32 @@ from FusedFAST import openFAST
 
 
     # in real life these come from an input file:
-'''filedict = {'ts_exe' : "/Users/pgraf/opt/windcode-7.31.13/TurbSim/build/TurbSim_glin64",
+'''
+filedict = {'ts_exe' : "/Users/pgraf/opt/windcode-7.31.13/TurbSim/build/TurbSim_glin64",
             'ts_dir' : "/Users/pgraf/work/wese/fatigue12-13/from_gordie/SparFAST3.orig/TurbSim",
             'ts_file' : "TurbSim.inp",
             'fst_exe' : "/Users/pgraf/opt/windcode-7.31.13/build/FAST_glin64",
             'fst_dir' : "/Users/pgraf/work/wese/fatigue12-13/from_gordie/SparFAST3.orig",
             'fst_file' : "NRELOffshrBsline5MW_Floating_OC3Hywind.fst",
-            'run_dir' : "run_dir"}'''
+            'run_dir' : "run_dir"}
+'''
+filedict = {'ts_exe' : "/Users/pgraf/opt/windcode-7.31.13/TurbSim/build/TurbSim_glin64",
+            'ts_dir' : "ModelFiles/OC3_FloatingOffshoreFiles/TurbSim",
+            'ts_file' : "TurbSim.inp",
+            'fst_exe' : "/Users/pgraf/opt/windcode-7.31.13/build/FAST_glin64",
+            'fst_dir' : "ModelFiles/OC3_FloatingOffshoreFiles/",
+            'fst_file' : "NRELOffshrBsline5MW_Floating_OC3Hywind.fst",
+            'run_dir' : "run_dir"}
 
-
+'''
 filedict = {'ts_exe' : "C:/Models/TurbSim/TurbSim64.exe",
             'ts_dir' : "C:/Python27/NREL-Models/WISDEM/AeroelasticSE/src/AeroelasticSE/InputFilesToWrite",
             'ts_file' : "turbsim_template.inp",
             'fst_exe' : "C:/Models/FAST/FAST_v7.02.00d-bjj.exe",
             'fst_dir' : "C:/Python27/NREL-Models/WISDEM/AeroelasticSE/src/AeroelasticSE/FAST_VT/OC3_Files",
             'fst_file' : "NRELOffshrBsline5MW_Monopile_RF.fst",
-            'run_dir' : "run_dir"}
+            'run_dir' : "run_dir"} 
+'''
 
 iec = FUSEDIECCaseIterator()
 
