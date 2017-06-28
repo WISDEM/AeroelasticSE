be64010b2fb53ef9b566fe3dc6c756553f0fb8c4
kilojoules <jaq8@humboldt.edu>
2017-06-28 15:23:28 -0600

    write same vartree that's read

--- src/AeroelasticSE/FAST_mdao/turbsim_vartrees.py
+++ src/AeroelasticSE/FAST_mdao/turbsim_vartrees.py
@@ -10,76 +10,76 @@ import numpy as np
 ##########################
 # Runtime Options
 class runtime_options(object):
-	def __init__(self):
-		self.echo = False
-		self.RandSeed1 = np.random.uniform(1, 1e8)
-		self.RandSeed2 = '"RANLUX"'
-		self.WrBHHTP = False
-		self.WrFHHTP = False
-		self.WrADHH = False
-		self.WrADFF = True
-		self.WrBLFF = False
-		self.WrADTWR = False
-		self.WrFMTFF = False
-		self.WrACT = False
-		self.Clockwise = True
-		self.ScaleIEC = 0
+    def __init__(self):
+         self.echo = False
+         self.RandSeed1 = np.random.uniform(1, 1e8)
+         self.RandSeed2 = '"RANLUX"'
+         self.WrBHHTP = False
+         self.WrFHHTP = False
+         self.WrADHH = False
+         self.WrADFF = True
+         self.WrBLFF = False
+         self.WrADTWR = False
+         self.WrFMTFF = False
+         self.WrACT = False
+         self.Clockwise = True
+         self.ScaleIEC = 0
 
 # Turbine/Model Specifications
 class tmspecs(object):
-	def __init__(self):
-		self.NumGrid_Z = 30
-		self.NumGrid_Y = 30
-		self.TimeStep = 0.05
-		self.AnalysisTime = 630
-		self.UsableTime = '"ALL"'
-		self.HubHt = 90
-		self.GridHeight = 138
-		self.GridWidth = 138
-		self.VFlowAng = 0
-		self.HFlowAng = 0
+    def __init__(self):
+         self.NumGrid_Z = 30
+         self.NumGrid_Y = 30
+         self.TimeStep = 0.05
+         self.AnalysisTime = 630
+         self.UsableTime = '"ALL"'
+         self.HubHt = 90
+         self.GridHeight = 138
+         self.GridWidth = 138
+         self.VFlowAng = 0
+         self.HFlowAng = 0
 
 # Meteorological Boundary Conditions
 class metboundconds(object):
-	def __init__(self):
-		self.TurbModel = '"USRINP"'
-		self.UserFile = 'turbulance_user.inp'
-		self.IECstandard = '"1-ED3"'
-		self.IECturbc = '"B"'
-		self.IEC_WindType = '"NTM"'
-		self.ETMc = '"default"'
-		self.WindProfileType = '"USR"'
-		self.ProfileFile = 'shear.profile'
-		self.RefHt = 90
-		self.URef = 12
-		self.ZJetMax = '"default"'
-		self.PLExp = '"default"'
-		self.Z0 = '"default"'
+    def __init__(self):
+         self.TurbModel = '"USRINP"'
+         self.UserFile = 'turbulance_user.inp'
+         self.IECstandard = '"1-ED3"'
+         self.IECturbc = '"B"'
+         self.IEC_WindType = '"NTM"'
+         self.ETMc = '"default"'
+         self.WindProfileType = '"USR"'
+         self.ProfileFile = 'shear.profile'
+         self.RefHt = 90
+         self.URef = 12
+         self.ZJetMax = '"default"'
+         self.PLExp = '"default"'
+         self.Z0 = '"default"'
 
 # Non-IEC Meteorological Boundary Conditions
 class noniecboundconds(object):
-	def __init__(self):
-		self.Latitude = '"default"'
-		self.RICH_NO = 0
-		self.UStar = '"default"'
-		self.ZI = '"default"'
-		self.PC_UW = -0.85
-		self.PC_UV = 0.15
-		self.PC_VW = -0.1
+    def __init__(self):
+         self.Latitude = '"default"'
+         self.RICH_NO = 0
+         self.UStar = '"default"'
+         self.ZI = '"default"'
+         self.PC_UW = -0.85
+         self.PC_UV = 0.15
+         self.PC_VW = -0.1
 
 # Spatial Coherence Parameters
 class spatialcoherance(object):
-	def __init__(self):
-		self.SCMod1 = '"GENERAL"'
-		self.SCMod2 = '"GENERAL"'
-		self.SCMod3 = '"GENERAL"'
-		self.InCDec1 = [13.75, 0.04]
-		self.InCDec2 = [9.85, 0.0015]
-		self.InCDec3 = [9.5, 0.003]
-		self.CohExp = 0.5
+    def __init__(self):
+         self.SCMod1 = '"GENERAL"'
+         self.SCMod2 = '"GENERAL"'
+         self.SCMod3 = '"GENERAL"'
+         self.InCDec1 = [13.75, 0.04]
+         self.InCDec2 = [9.85, 0.0015]
+         self.InCDec3 = [9.5, 0.003]
+         self.CohExp = 0.5
 # Coherent Turbulence Scaling Parameters
 class coherentTurbulence(object):
-     def __init__(self):
+    def __init__(self):
          self.CTEventPath = '"Y:\Wind\Archive\Public\Projects\KH_Billow\EventData"'
          self.CTEventFile = '"Random"'
          self.Randomize = True
@@ -89,8 +89,7 @@ class coherentTurbulence(object):
          self.CTStartTime = 30
 
 class turbsiminputs(object):
-
-     def __init__(self):
+    def __init__(self):
          self.runtime_options = runtime_options()
          self.tmspecs = tmspecs()
          self.metboundconds = metboundconds()
--- src/AeroelasticSE/FAST_mdao/turbsim_writer.py
+++ src/AeroelasticSE/FAST_mdao/turbsim_writer.py
@@ -1,94 +1,114 @@
 from turbsim_vartrees import turbsiminputs
+from turbulence_spectrum import turb_specs
+from wind_profile_writer import write_wind
 class TurbsimBuilder(turbsiminputs):
     def __init__(self):
-         super(TurbsimBuilder, self).__init__()
+         self.turbsim_vt = turbsiminputs()
          self.tsim_input_file = 'turbsim_default.in'
          self.tsim_turbulence_file = 'turbulence_default.in'
          self.tsim_profile_file = 'default.shear'
+ 
+         # Turbulence file parameters
+         self.wind_speed = 8.
+         self.L_u = 2.54e+02
+         self.L_v=1.635e+02
+         self.L_w=4.7e+01
+         self.sigma_u=2.65e+00
+         self.sigma_v=1.8e0
+         self.sigma_w=1.525e0
+
     def execute(self):
+
+         # Write turbulence file
+         turb_specs(V_ref=self.wind_speed, L_u=self.L_u, L_v=self.L_v, L_w=self.L_w, sigma_u=self.sigma_u,
+                    sigma_v=self.sigma_v, sigma_w=self.sigma_w)
+
+         # Write profile file
+         write_wind(V_ref=self.wind_speed, alpha=self.shear_exponent, beta=self.veer, Z_hub=self.turbsim_vt.tmspecs.HubHt)
+
          tsinp = open(self.tsim_input_file, 'w')
          tsinp.write("-----\n")
          tsinp.write("-----\n")
          tsinp.write("-----\n")
 
          # runtime options
-         tsinp.write("{}\n".format(self.runtime_options.echo))
-         tsinp.write("{}\n".format(int(self.runtime_options.RandSeed1)))
-         tsinp.write("{}\n".format(self.runtime_options.RandSeed2))
-         tsinp.write("{}\n".format(self.runtime_options.WrBHHTP))
-         tsinp.write("{}\n".format(self.runtime_options.WrFHHTP))
-         tsinp.write("{}\n".format(self.runtime_options.WrADHH))
-         tsinp.write("{}\n".format(self.runtime_options.WrADFF))
-         tsinp.write("{}\n".format(self.runtime_options.WrBLFF))
-         tsinp.write("{}\n".format(self.runtime_options.WrADTWR))
-         tsinp.write("{}\n".format(self.runtime_options.WrFMTFF))
-         tsinp.write("{}\n".format(self.runtime_options.WrACT))
-         tsinp.write("{}\n".format(self.runtime_options.Clockwise))
-         tsinp.write("{}\n".format(self.runtime_options.ScaleIEC))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.echo))
+         tsinp.write("{}\n".format(int(self.turbsim_vt.runtime_options.RandSeed1)))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.RandSeed2))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.WrBHHTP))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.WrFHHTP))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.WrADHH))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.WrADFF))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.WrBLFF))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.WrADTWR))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.WrFMTFF))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.WrACT))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.Clockwise))
+         tsinp.write("{}\n".format(self.turbsim_vt.runtime_options.ScaleIEC))
 
          # Turbine/Model Specifications
          tsinp.write("\n")
          tsinp.write("----\n")
-         tsinp.write("{}\n".format(self.tmspecs.NumGrid_Z))
-         tsinp.write("{}\n".format(self.tmspecs.NumGrid_Y))
-         tsinp.write("{}\n".format(self.tmspecs.TimeStep))
-         tsinp.write("{}\n".format(self.tmspecs.AnalysisTime))
-         tsinp.write("{}\n".format(self.tmspecs.UsableTime))
-         tsinp.write("{}\n".format(self.tmspecs.HubHt))
-         tsinp.write("{}\n".format(self.tmspecs.GridHeight))
-         tsinp.write("{}\n".format(self.tmspecs.GridWidth))
-         tsinp.write("{}\n".format(self.tmspecs.VFlowAng))
-         tsinp.write("{}\n".format(self.tmspecs.HFlowAng))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.NumGrid_Z))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.NumGrid_Y))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.TimeStep))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.AnalysisTime))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.UsableTime))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.HubHt))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.GridHeight))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.GridWidth))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.VFlowAng))
+         tsinp.write("{}\n".format(self.turbsim_vt.tmspecs.HFlowAng))
 
          # Meteorological Boundary Conditions
          tsinp.write("\n")
          tsinp.write("----\n")
-         tsinp.write("{}\n".format(self.metboundconds.TurbModel))
-         tsinp.write("{}\n".format(self.metboundconds.UserFile))
-         tsinp.write("{}\n".format(self.metboundconds.IECstandard))
-         tsinp.write("{}\n".format(self.metboundconds.IECturbc))
-         tsinp.write("{}\n".format(self.metboundconds.IEC_WindType))
-         tsinp.write("{}\n".format(self.metboundconds.ETMc))
-         tsinp.write("{}\n".format(self.metboundconds.WindProfileType))
-         tsinp.write("{}\n".format(self.metboundconds.ProfileFile))
-         tsinp.write("{}\n".format(self.metboundconds.RefHt))
-         tsinp.write("{}\n".format(self.metboundconds.URef))
-         tsinp.write("{}\n".format(self.metboundconds.ZJetMax))
-         tsinp.write("{}\n".format(self.metboundconds.PLExp))
-         tsinp.write("{}\n".format(self.metboundconds.Z0))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.TurbModel))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.UserFile))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.IECstandard))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.IECturbc))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.IEC_WindType))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.ETMc))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.WindProfileType))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.ProfileFile))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.RefHt))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.URef))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.ZJetMax))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.PLExp))
+         tsinp.write("{}\n".format(self.turbsim_vt.metboundconds.Z0))
 
          # Non-IEC Meteorological Boundary Conditions
          tsinp.write("\n")
          tsinp.write("----\n")
-         tsinp.write("{}\n".format(self.noniecboundconds.Latitude))
-         tsinp.write("{}\n".format(self.noniecboundconds.RICH_NO))
-         tsinp.write("{}\n".format(self.noniecboundconds.UStar))
-         tsinp.write("{}\n".format(self.noniecboundconds.ZI))
-         tsinp.write("{}\n".format(self.noniecboundconds.PC_UW))
-         tsinp.write("{}\n".format(self.noniecboundconds.PC_UV))
-         tsinp.write("{}\n".format(self.noniecboundconds.PC_VW))
+         tsinp.write("{}\n".format(self.turbsim_vt.noniecboundconds.Latitude))
+         tsinp.write("{}\n".format(self.turbsim_vt.noniecboundconds.RICH_NO))
+         tsinp.write("{}\n".format(self.turbsim_vt.noniecboundconds.UStar))
+         tsinp.write("{}\n".format(self.turbsim_vt.noniecboundconds.ZI))
+         tsinp.write("{}\n".format(self.turbsim_vt.noniecboundconds.PC_UW))
+         tsinp.write("{}\n".format(self.turbsim_vt.noniecboundconds.PC_UV))
+         tsinp.write("{}\n".format(self.turbsim_vt.noniecboundconds.PC_VW))
 
          # Spatial Coherence Parameters
          tsinp.write("\n")
          tsinp.write("----\n")
-         tsinp.write("{}\n".format(self.spatialcoherance.SCMod1))
-         tsinp.write("{}\n".format(self.spatialcoherance.SCMod2))
-         tsinp.write("{}\n".format(self.spatialcoherance.SCMod3))
-         tsinp.write("{}\n".format(self.spatialcoherance.InCDec1))
-         tsinp.write("{}\n".format(self.spatialcoherance.InCDec2))
-         tsinp.write("{}\n".format(self.spatialcoherance.InCDec3))
-         tsinp.write("{}\n".format(self.spatialcoherance.CohExp))
+         tsinp.write("{}\n".format(self.turbsim_vt.spatialcoherance.SCMod1))
+         tsinp.write("{}\n".format(self.turbsim_vt.spatialcoherance.SCMod2))
+         tsinp.write("{}\n".format(self.turbsim_vt.spatialcoherance.SCMod3))
+         tsinp.write("{}\n".format(self.turbsim_vt.spatialcoherance.InCDec1))
+         tsinp.write("{}\n".format(self.turbsim_vt.spatialcoherance.InCDec2))
+         tsinp.write("{}\n".format(self.turbsim_vt.spatialcoherance.InCDec3))
+         tsinp.write("{}\n".format(self.turbsim_vt.spatialcoherance.CohExp))
 
          # Coherent Turbulence Scaling Parameters
          tsinp.write("\n")
          tsinp.write("----\n")
-         tsinp.write("{}\n".format(self.coherentTurbulence.CTEventPath))
-         tsinp.write("{}\n".format(self.coherentTurbulence.CTEventFile))
-         tsinp.write("{}\n".format(self.coherentTurbulence.Randomize))
-         tsinp.write("{}\n".format(self.coherentTurbulence.DistScl))
-         tsinp.write("{}\n".format(self.coherentTurbulence.CTLy))
-         tsinp.write("{}\n".format(self.coherentTurbulence.CTLz))
-         tsinp.write("{}\n".format(self.coherentTurbulence.CTStartTime))
+         tsinp.write("{}\n".format(self.turbsim_vt.coherentTurbulence.CTEventPath))
+         tsinp.write("{}\n".format(self.turbsim_vt.coherentTurbulence.CTEventFile))
+         tsinp.write("{}\n".format(self.turbsim_vt.coherentTurbulence.Randomize))
+         tsinp.write("{}\n".format(self.turbsim_vt.coherentTurbulence.DistScl))
+         tsinp.write("{}\n".format(self.turbsim_vt.coherentTurbulence.CTLy))
+         tsinp.write("{}\n".format(self.turbsim_vt.coherentTurbulence.CTLz))
+         tsinp.write("{}\n".format(self.turbsim_vt.coherentTurbulence.CTStartTime))
 
 
 
