9d3bd379eb9a49b12bc160b86d3f5a3aef1510dd
kilojoules <jaq8@humboldt.edu>
2017-06-28 17:05:27 -0600

    user-defined turbulence files

--- src/AeroelasticSE/Turbsim_mdao/turbulence_spectrum.py
+++ src/AeroelasticSE/Turbsim_mdao/turbulence_spectrum.py
@@ -2,6 +2,8 @@ import numpy as np
 import os
 import pandas
 from collections import OrderedDict
+import AeroelasticSE
+templateFile = AeroelasticSE.__path__[0]
 def turb_specs(V_ref, L_u, L_v, L_w, sigma_u, sigma_v, sigma_w):
     
     f=(np.array([np.arange(0.0015873015873015873015873015873, 100.00001, 0.0015873015873015873015873015873)])).T
@@ -18,10 +20,10 @@ def turb_specs(V_ref, L_u, L_v, L_w, sigma_u, sigma_v, sigma_w):
         W[i,0]= (4*L_w/V_ref)*sigma_w**2/((1+6*f[i,0]*L_w/V_ref)**(5/3))
         
     df=pandas.DataFrame({'Frequency (Hz)':f[:,0],'u-component PSD (m^2/s)': U[:,0],'v-component PSD (m^2/s)': V[:,0],'w-component PSD (m^2/s)':W[:,0]})
-    with open('UsrSpec_'+str(V_ref)+'ms.inp','r') as f:
+    with open(templateFile, 'r') as f:
         get_all=f.readlines() 
     
-    name=os.sep.join(['Row_' + str(ii)+'_BIN_'+str(b),'Turbsim_inputs',str(V_ref)+'ms_Turbulence_row_'+str(ii)+'.inp'])
+    name=os.sep.join(['default_turbulence.inp'])
     
         
     with open(name,'w') as f:
