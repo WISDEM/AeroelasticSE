63ebdf3d1979aa4992b4779243620c4c3144f8f7
Evan Gaertner <evan.gaertner@gmail.com>
2019-05-15 09:25:56 -0600

    point to point mpi communication

--- src/AeroelasticSE/runFAST_pywrapper.py
+++ src/AeroelasticSE/runFAST_pywrapper.py
@@ -216,8 +216,8 @@ class runFAST_pywrapper_batch(object):
             idx_s    = i*size
             idx_e    = min((i+1)*size, N_cases)
 
-            for j, var_vals in enumerate(case_data_all[idx_s:idx_e]):
-                data   = [gen_windfile, [iecwind, IEC_WindType, change_vars, var_vals]]
+            for j, case_data in enumerate(case_data_all[idx_s:idx_e]):
+                data   = [eval_multi, case_data]
                 rank_j = sub_ranks[j]
                 comm.send(data, dest=rank_j, tag=0)
 
